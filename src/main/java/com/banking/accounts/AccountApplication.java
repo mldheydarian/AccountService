1ae2dcd3fa270982588b2912b069cbce3c914617
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-14 15:11:25 -0700

    Project Resource Files Commit

---
+++ src/main/java/com/banking/accounts/AccountApplication.java
@@ -0,0 +1,18 @@
+package com.banking.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Currency;
+import java.util.Locale;
+
+
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+
+    }
+
+}
