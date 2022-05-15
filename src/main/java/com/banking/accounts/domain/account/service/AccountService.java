c93873c8f68ca052ada5e2814ed63dcb9e52c815
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-15 12:34:14 -0700

    Account Service class and Interface Added

---
+++ src/main/java/com/banking/accounts/domain/account/service/AccountService.java
@@ -0,0 +1,45 @@
+package com.banking.accounts.domain.account.service;
+
+import com.banking.accounts.domain.account.dto.AccountDto;
+import com.banking.accounts.domain.account.entity.Account;
+import com.banking.accounts.domain.account.repository.IAccountRepository;
+import com.banking.accounts.domain.transaction.dto.TransactionDto;
+import com.banking.accounts.domain.transaction.entity.EnumTransactionStatus;
+import com.banking.accounts.domain.transaction.entity.EnumTransactionType;
+import com.banking.accounts.domain.transaction.entity.Transaction;
+import com.banking.accounts.domain.transaction.service.ITransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+
+
+@Service
+@AllArgsConstructor
+public class AccountService implements IAccountService{
+
+
+    final IAccountFactory accountFactory;
+    final IAccountRepository accountRepository;
+    final ITransactionService transactionService;
+
+    @Transactional
+    public Account createAccount(AccountDto accountDto)
+    {
+        Account newAccount = accountFactory.createAccount(accountDto);
+        accountRepository.save(newAccount);
+
+        if (newAccount.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) // if amount is larger than 0
+        {
+            TransactionDto transactionDto = new TransactionDto(EnumTransactionStatus.Posted,"Account Initializing Deposit", EnumTransactionType.Deposit,newAccount.getId(),accountDto.getInitialCredit());
+
+            Transaction initialTransaction = transactionService.createTransaction(transactionDto);
+            newAccount.updateBalance(initialTransaction.getFinalAmount());
+            initialTransaction.updateStatus(EnumTransactionStatus.Confirmed);
+            accountRepository.save(newAccount);
+        }
+
+        return newAccount;
+    }
+}
---
+++ src/main/java/com/banking/accounts/domain/account/service/IAccountService.java
@@ -0,0 +1,10 @@
+package com.banking.accounts.domain.account.service;
+
+import com.banking.accounts.domain.account.dto.AccountDto;
+import com.banking.accounts.domain.account.entity.Account;
+import com.banking.accounts.domain.transaction.entity.Transaction;
+
+
+public interface IAccountService {
+    Account createAccount(AccountDto accountDto) ;
+}
