784202ef467cfc2c3f5b1f051db6a1af12bf4347
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-15 12:44:07 -0700

    Account Repository Added

--- src/main/java/com/banking/accounts/domain/account/repository/IAccountRepository.java
+++ src/main/java/com/banking/accounts/domain/account/repository/IAccountRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Integer> {
+public interface IAccountRepository extends JpaRepository<Account, Integer> {
 
 
 }
