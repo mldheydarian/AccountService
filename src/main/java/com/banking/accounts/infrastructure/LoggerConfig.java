8d85363a63b78b82919dedc9aeb3b5648b517a38
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-17 09:01:58 -0700

    Log4J2 Added

---
+++ src/main/java/com/banking/accounts/infrastructure/LoggerConfig.java
@@ -0,0 +1,19 @@
+package com.banking.accounts.infrastructure;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class LoggerConfig {
+
+    @Bean
+    @Scope("prototype")
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        Class<?> classOnWired = injectionPoint.getMember().getDeclaringClass();
+        return LogManager.getLogger(classOnWired);
+    }
+}
