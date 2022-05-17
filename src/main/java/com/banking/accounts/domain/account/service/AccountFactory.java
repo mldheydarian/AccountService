ca217b2c55417497404d703af86b4e06b42aa3c9
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-17 09:44:13 -0700

    AccountFactory and AccountFactoryTest Updated after adding new Repository

--- src/main/java/com/banking/accounts/domain/account/service/AccountFactory.java
+++ src/main/java/com/banking/accounts/domain/account/service/AccountFactory.java
@@ -1,23 +1,48 @@
 package com.banking.accounts.domain.account.service;
 
+import com.banking.accounts.application.exception.RecordNotFoundException;
 import com.banking.accounts.domain.account.dto.AccountDto;
-import com.banking.accounts.domain.account.entity.Account;
-import com.banking.accounts.domain.account.entity.CurrentAccount;
-import com.banking.accounts.domain.account.entity.SavingAccount;
+import com.banking.accounts.domain.account.entity.*;
+import com.banking.accounts.domain.account.repository.IAccountTypeRepository;
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
-@Component
-public class AccountFactory implements IAccountFactory {
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+
+public class AccountFactory implements IAccountFactory  {
+
+    public AccountFactory(IAccountTypeRepository accountTypeRepository) {
+        this.accountTypeRepository = accountTypeRepository;
+    }
+    private final IAccountTypeRepository accountTypeRepository;
 
 
     public Account createAccount(AccountDto accountDto) {
+
+        List<AccountType> accountTypeList = accountTypeRepository.findAll();
         switch (accountDto.getEnumAccountType()) {
             case CurrentAccount:
-                return new CurrentAccount(accountDto);
+                AccountType currentAccountType  = accountTypeList.stream()
+                        .filter(accountType -> EnumAccountType.CurrentAccount.equals(accountType.getEnumAccountType()))
+                        .findAny()
+                        .orElse(null);
+                return new CurrentAccount(accountDto,currentAccountType);
+
             case SavingAccount:
-                return new SavingAccount(accountDto);
+                AccountType savingAccountType = accountTypeList.stream()
+                        .filter(accountType -> EnumAccountType.SavingAccount.equals(accountType.getEnumAccountType()))
+                        .findAny()
+                        .orElse(null);
+                return new SavingAccount(accountDto, savingAccountType);
+
         }
-        return null; // TODO: throw exception
+        throw new RecordNotFoundException("This Type of Account Is not Ready now");
     }
 }
--- src/test/java/com/banking/accounts/domain/account/service/AccountFactoryTest.java
+++ src/test/java/com/banking/accounts/domain/account/service/AccountFactoryTest.java
@@ -1,39 +1,53 @@
 package com.banking.accounts.domain.account.service;
 
 import com.banking.accounts.domain.account.dto.AccountDto;
-import com.banking.accounts.domain.account.entity.CurrentAccount;
-import com.banking.accounts.domain.account.entity.EnumAccountType;
-import com.banking.accounts.domain.account.entity.SavingAccount;
+import com.banking.accounts.domain.account.entity.*;
+import com.banking.accounts.domain.account.repository.IAccountRepository;
+import com.banking.accounts.domain.account.repository.IAccountTypeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 
 class AccountFactoryTest {
 
+    @MockBean
+    IAccountTypeRepository mockAccountTypeRepository;
+
     IAccountFactory accountFactory;
     AccountDto currentAccountDto;
     AccountDto savingAccountDto;
-
+    private List<AccountType> accountTypeList;
 
     @BeforeEach
     void setUp() {
-        accountFactory =new AccountFactory();
+        accountFactory = new AccountFactory(mockAccountTypeRepository);
         Calendar calendar = Calendar.getInstance();
         calendar.set(1991, 9, 21 );
         currentAccountDto = new AccountDto(1, EnumAccountType.CurrentAccount, BigDecimal.valueOf(10),"Arman.heydarian@gmail.com", calendar.getTime());
         savingAccountDto = new AccountDto(1, EnumAccountType.SavingAccount, BigDecimal.valueOf(10),"Arman.heydarian@gmail.com", calendar.getTime());
-
+        currentAccountDto = new AccountDto(1, EnumAccountType.CurrentAccount, BigDecimal.valueOf(10),"Arman.heydarian@gmail.com", calendar.getTime());
+        accountTypeList = new ArrayList<AccountType>(){{
+             add(new AccountType((long)1,EnumAccountType.CurrentAccount,BigDecimal.valueOf(100)));
+             add(new AccountType((long)2,EnumAccountType.SavingAccount,BigDecimal.valueOf(100)));
+        }};
     }
 
     @Test
     void createCurrentAccount() {
+        Mockito.when(mockAccountTypeRepository.findAll()).thenReturn(accountTypeList);
         assertInstanceOf(CurrentAccount.class , accountFactory.createAccount(currentAccountDto));
     }
     @Test
     void createDepositAccount() {
+        Mockito.when(mockAccountTypeRepository.findAll()).thenReturn(accountTypeList);
         assertInstanceOf(SavingAccount.class , accountFactory.createAccount(savingAccountDto));
     }
 
