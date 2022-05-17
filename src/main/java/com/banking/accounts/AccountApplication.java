abb8f197f204f79ca63598f421c42df8e2937b31
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-17 14:39:47 -0700

    Swagger Configuration Added

--- src/main/java/com/banking/accounts/AccountApplication.java
+++ src/main/java/com/banking/accounts/AccountApplication.java
@@ -1,14 +1,19 @@
 package com.banking.accounts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Currency;
 import java.util.Locale;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class AccountApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
