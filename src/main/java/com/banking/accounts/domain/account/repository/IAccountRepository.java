b2ab70564bcbfea82990dfd52ee0a17e4886d348
ArmanHeydarian <arman.heydarian@gmail.com>
2022-05-17 14:36:06 -0700

    IAccountRepository added

--- src/main/java/com/banking/accounts/domain/account/repository/IAccountRepository.java
+++ src/main/java/com/banking/accounts/domain/account/repository/IAccountRepository.java
@@ -4,9 +4,12 @@ import com.banking.accounts.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Integer> {
 
-
+    List<Account> findByCustomerId(@NotNull Integer customerId);
 }
 
